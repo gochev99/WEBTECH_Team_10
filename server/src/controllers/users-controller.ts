60f39459519feee989099cdff3fd3038a128fcb1
simonakkirilova <81169214+simonakkirilova@users.noreply.github.com>
2021-06-14 08:39:35 +0300

    Add files via upload

---
+++ server/src/controllers/users-controller.ts
@@ -0,0 +1,68 @@
+import * as bcrypt from 'bcrypt';
+import * as _ from 'lodash';
+import * as mongoose from 'mongoose';
+import IUser from '../interfaces/user';
+import { read, write } from '../utils/file-utils';
+import { UserDocument, User } from '../models/user';
+
+const filePath: string = '../resources';
+const filename: string = '/users.json';
+
+class UserController {
+    construct() {} 
+
+    public async createUser(user: IUser): Promise<void> {
+        bcrypt.hash(user.password, 10, async (error: Error, hash: string) => {
+            if (error) {
+                return;
+            }
+
+            user.password = hash;
+
+            const newUser = new User({
+                _id: new mongoose.Types.ObjectId(),
+                username: user.username,
+                password: user.password, 
+                email: user.email
+            });
+
+            await newUser.save();
+        });
+    }
+
+    public async findUser(user: string): Promise<UserDocument> {
+        
+        return User.findOne({ username: user }).exec();
+    }
+
+    private async readUsers(): Promise<{ [users: string]: IUser[] }> {
+        const users: string = await read(filePath, filename);
+        const usersObject: { [users: string]: IUser[] } = JSON.parse(users);
+
+        return usersObject;
+    }
+
+    public validateUser(user: IUser, confirmPassword: string): string[] {
+        const errors: string[] = [];
+
+        if (!user.username) {
+            errors.push('Please input user name');
+        } else if (user.username.length > 15) {
+            errors.push('User name must be less than 15 symbols');
+        }
+
+        if (!user.password) {
+            errors.push('Please input password');
+        } else if (!user.password.match(/[a-zA-z0-9]?/)) {
+            errors.push('Password must contain letters and digits');
+        } else if (user.password.length < 5) {
+            errors.push('Password must be longer than 5 symbols');
+        } else if (confirmPassword && user.password !== confirmPassword) {
+            errors.push('Passwords must match.');
+        }
+
+        return errors;
+    }
+}
+
+export default UserController;
