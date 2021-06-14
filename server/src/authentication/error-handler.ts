639d6271d63ddc2c7c94e8d482e14efb5664e7b8
Avgustin Gochev <tinkoto99@gmail.com>
2021-06-14 10:19:47 +0300

    .

--- server/src/authentication/error-handler.ts
+++ server/src/authentication/error-handler.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 
-const errorCatch = (middlewareFn: (req: Request, res: Response, next?) => void) => {
+const errorCatch = (authF: (req: Request, res: Response, next?) => void) => {
   const handleError = async (req: Request, res: Response, next) => {
     try {
-      await middlewareFn(req, res, next);
+      await authF(req, res, next);
     } catch (error) {
       res.status(400).json(error);
     }
