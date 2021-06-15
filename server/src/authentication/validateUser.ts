31beec409f333c52ee3413a9abd2f0a456ead20b
Avgustin Gochev <tinkoto99@gmail.com>
2021-06-15 11:18:00 +0300

    .

--- server/src/authentication/validateUser.ts
+++ server/src/authentication/validateUser.ts
@@ -1,19 +1,19 @@
-import { Request, Response } from 'express';
-import UserController from '../controllers/users-controller';
-import IUser from '../interfaces/user';
+// import { Request, Response } from 'express';
+// import UserController from '../controllers/users-controller';
+// import IUser from '../interfaces/user';
 
-const controller: UserController = new UserController();
+// const controller: UserController = new UserController();
 
-const validateUser = (request: Request, response: Response, next: () => void): void => {
-    const user: IUser = request.body;
+// const validateUser = (request: Request, response: Response, next: () => void): void => {
+//     const user: IUser = request.body;
 
-    const isValidUser: string[] = controller.validateUser(user, request.body.confirmPassword);
+//     const isValidUser: string[] = controller.validateUser(user, request.body.confirmPassword);
     
-    if (isValidUser.length > 0) {
-        response.status(400).json({error: isValidUser});
-    } else {
-        next();
-    }
-};
+//     if (isValidUser.length > 0) {
+//         response.status(400).json({error: isValidUser});
+//     } else {
+//         next();
+//     }
+// };
 
-export default validateUser;
+// export default validateUser;
